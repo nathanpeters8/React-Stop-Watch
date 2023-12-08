cd68c3e7e80f11d63a53ef34c0a9871616b6322a
Nathan Peters <peters.nate8@gmail.com>
2023-12-08 15:09:20 -0700

    remake app with hooks

--- script.js
+++ script.js
@@ -1,76 +1,62 @@
-class StopWatch extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      timePassedInMilliSeconds: 0,
-    };
+const { useState, useEffect, useRef } = React;
 
-    // timer stored as prop of class object because it needs used in both start and stop methods
-    this.timer = null;
+const StopWatch = () => {
+  
+  const [timePassed, setTimePassed] = useState(0);
 
-    // bind methods to object
-    this.start = this.start.bind(this);
-    this.stop = this.stop.bind(this);
-    this.reset = this.reset.bind(this);
-  }
-
+  // make timer a ref with initial state of null
+  const timer = useRef(null);
+  
   // calculate elapsed time since user clicks start button
-  start() {
-    //check if timer does not already exists
-    if(!this.timer) {
+  const start = () => {
+    // use .current to access the timer vallue
+    if(!timer.current) {
       // get start time
       let startTime = Date.now();
   
       // interval function that runs every 250ms
-      this.timer = setInterval(() => {
+      timer.current = setInterval(() => {
         // get elapsed time between stop time and start time
+        console.log('timePassed:', timePassed);
         const stopTime = Date.now();
-        const timePassedInMilliSeconds = stopTime - startTime + this.state.timePassedInMilliSeconds;
   
-        // update the state of elapsed time
-        this.setState({timePassedInMilliSeconds});
+        // use callback in setState to get latest state value
+        setTimePassed(timePassed => stopTime - startTime + timePassed);
         
         //reset start time to stop time
         startTime = stopTime;
   
-      }, 250);
+      }, 1000);
     }
   }
   
   // stop the timer on stop button click
-  stop() {
+  const stop = ()  => {
+    console.log('stop', timer.current);
     // clear interval and set timer to null
-    window.clearInterval(this.timer);
-    this.timer = null;
+    window.clearInterval(timer.current);
+    timer.current = null;
   }
 
   // reset timer on reset button click
-  reset() {
-    // stop timer
-    this.stop();
-
-    // update timer value to 0
-    this.setState({
-      timePassedInMilliSeconds : 0
-    });
+  const reset = () => {
+    stop();
+    setTimePassed(0);
   }
 
   // render the React component in the DOM
-  render() {
-    return (
-      <div>
-        <h2 className='border px-3 py-4 rounded my-3 mx-auto text-center' style={{ maxWidth: '300px' }}>
-          {Math.floor(this.state.timePassedInMilliSeconds / 1000)} s
-        </h2>
-        <div className='d-flex justify-content-center'>
-          <button className='btn btn-outline-primary mr-2' onClick={this.start}>start</button>
-          <button className='btn btn-outline-danger mr-2' onClick={this.stop}>stop</button>
-          <button className='btn btn-outline-warning' onClick={this.reset}>reset</button>
-        </div>
+  return (
+    <div>
+      <h2 className='border px-3 py-4 rounded my-3 mx-auto text-center' style={{ maxWidth: '300px' }}>
+        {Math.floor(timePassed / 1000)} s
+      </h2>
+      <div className='d-flex justify-content-center'>
+        <button className='btn btn-outline-primary mr-2' onClick={start}>start</button>
+        <button className='btn btn-outline-danger mr-2' onClick={stop}>stop</button>
+        <button className='btn btn-outline-warning' onClick={reset}>reset</button>
       </div>
-    );
-  }
-
+    </div>
+  );
 }
 
 const container = document.getElementById('root');
